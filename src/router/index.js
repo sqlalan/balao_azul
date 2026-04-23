a45f4ba416077fbcad80f8b039f96bd57544eb60
Alan Muterle da Silva <sqlalan@gmail.com>
2026-04-22 21:02:31 -0300

    vue router

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import home from '../views/home.vue'
+import sobre from '/./views/sobre.vue'
+import contato from '../views/contato.vue'
+const routes = [
+{ path: '/', component: home },
+{ path: '/sobre', component: sobre },
+{ path: '/contato', component: contato },
+]
+const router = createRouter({
+history: createWebHistory(),
+routes
+})
+export default router
---
+++ src/views/home.vue
@@ -0,0 +1,11 @@
+<template>
+<div>
+<h1>HOME</h1>
+<p>Conteúdo da página Home.</p>
+</div>
+</template>
+<script>
+export default {
+name: 'home'
+}
+</script>
